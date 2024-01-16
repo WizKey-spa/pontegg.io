c44d52c72b8917dcdda539186f5589992dc4d5a0
K1000 <selwak@gmail.com>
2024-01-16 12:11:23 +0100

    Delete email-related files

--- src/emails/events/SendEmailEvent.ts
+++
@@ -1,6 +0,0 @@
-import { IEvent } from '@nestjs/cqrs';
-import { SendMailOptions } from 'nodemailer';
-
-export class SendEmailEvent implements IEvent {
-  constructor(public msg: SendMailOptions) {}
-}
