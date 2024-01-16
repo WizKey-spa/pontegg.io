63af3737017348960e866eb4e9f4e99d4405b936
K1000 <selwak@gmail.com>
2024-01-16 15:22:24 +0100

    Add mocks for testing
    This commit adds a new file "mocks.ts" which contains mock functions and objects for testing purposes. The file includes functions to create mock readable streams, mock buffers, and a mock logging object. These mocks will be useful for writing unit tests.

---
+++ test/mocks/mocks.ts
@@ -0,0 +1,23 @@
+import { Readable } from 'stream';
+import { testFileContent } from '../testdata';
+
+export const getMockReadable = () => {
+  const mockReadable = new Readable();
+  mockReadable.push(testFileContent);
+  mockReadable.push(null);
+  return mockReadable;
+};
+
+export const getMockBuffer = () => {
+  const mockReadable = new Readable();
+  mockReadable.push(testFileContent);
+  mockReadable.push(null);
+  return mockReadable;
+};
+
+export const logMock = {
+  setContext: jest.fn(),
+  debug: jest.fn(),
+  info: jest.fn(),
+  error: jest.fn(),
+};
